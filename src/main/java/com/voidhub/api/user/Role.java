59c535dbbb42a6c7faba80b111533247952ceb09
DaanSchar <Daan.Schaer@Gmail.com>
2022-12-23 01:02:46 +0100

    updated Role enum

--- src/main/java/com/voidhub/api/user/Role.java
+++ src/main/java/com/voidhub/api/user/Role.java
@@ -1,20 +1,35 @@
 package com.voidhub.api.user;
 
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
 public enum Role {
 
-    MEMBER("ROLE_MEMBER"),
-    ADMIN("ROLE_ADMIN");
+    MEMBER(
+            "ROLE_MEMBER",
+            "self:write",
+            "user:read"
+    ),
+    ADMIN(
+            "ROLE_ADMIN",
+            "self:write",
+            "user:read",
+            "user:write"
+    );
 
-    private final String name;
+    private final List<SimpleGrantedAuthority> authorities;
 
-    Role(String name) {
-        this.name = name;
+    Role(String... authorities) {
+        this.authorities = Arrays.stream(authorities).map(SimpleGrantedAuthority::new).toList();
     }
 
     @Override
     public String toString() {
-        return name;
+        return authorities.get(0).getAuthority();
     }
-
 
 }
