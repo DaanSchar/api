4f73a11401e6602a8b9886d47dfde1f6fbf97b40
DaanSchar <Daan.Schaer@Gmail.com>
2022-12-23 01:09:51 +0100

    added EventHost role

--- src/main/java/com/voidhub/api/user/Role.java
+++ src/main/java/com/voidhub/api/user/Role.java
@@ -3,14 +3,18 @@ package com.voidhub.api.user;
 import lombok.Getter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Getter
 public enum Role {
 
     MEMBER(
             "ROLE_MEMBER",
+            "self:write",
+            "user:read"
+    ),
+    EventHost(
+            "ROLE_EVENT_HOST",
             "self:write",
             "user:read"
     ),
@@ -24,7 +28,9 @@ public enum Role {
     private final List<SimpleGrantedAuthority> authorities;
 
     Role(String... authorities) {
-        this.authorities = Arrays.stream(authorities).map(SimpleGrantedAuthority::new).toList();
+        this.authorities = Arrays.stream(authorities)
+                .map(SimpleGrantedAuthority::new)
+                .toList();
     }
 
     @Override
