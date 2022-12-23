1afbfa1ca975fee6c73d170325d9560679e59180
DaanSchar <Daan.Schaer@Gmail.com>
2022-12-23 21:43:22 +0100

    chore: added NewEventForm and EventDto

---
+++ src/main/java/com/voidhub/api/event/EventDto.java
@@ -0,0 +1,32 @@
+package com.voidhub.api.event;
+
+import com.voidhub.api.user.UserDto;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+public class EventDto {
+
+    public EventDto(Event event) {
+        this.id = event.getId();
+        this.title = event.getTitle();
+        this.shortDescription = event.getShortDescription();
+        this.fullDescription = event.getFullDescription();
+        this.creationDate = event.getCreationDate();
+        this.applicationDeadline = event.getApplicationDeadline();
+        this.startingDate = event.getStartingDate();
+        this.creator = new UserDto(event.getCreator());
+    }
+
+    private String title;
+    private UUID id;
+    private String shortDescription;
+    private String fullDescription;
+    private Date creationDate;
+    private Date applicationDeadline;
+    private Date startingDate;
+    private UserDto creator;
+
+}
